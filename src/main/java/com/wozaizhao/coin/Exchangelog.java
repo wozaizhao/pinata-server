17059ae9c6e56605bbc0c3f27f9325c89dca0150
Alberto <xchao@Albertos-MacBook-Pro.local>
2018-01-03 20:07:43 +0800

    getlatest

---
+++ src/main/java/com/wozaizhao/coin/Exchangelog.java
@@ -0,0 +1,43 @@
+package com.wozaizhao.coin;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Exchangelog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private Date start;
+
+    private Date end;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+}
--- src/main/java/com/wozaizhao/coin/MainController.java
+++ src/main/java/com/wozaizhao/coin/MainController.java
@@ -6,11 +6,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.*;
+
 @RestController
 public class MainController {
 
     @Autowired
     private ExchangeRepository exchangeRepository;
+
+    @Autowired
+    private ExchangelogRepository exchangelogRepository;
+
 
     @RequestMapping(path = "/")
     public String root() {
@@ -23,9 +29,33 @@ public class MainController {
         return exchangeRepository.findAll();
     }
 
+    private Iterable<Exchangelog> getAllLog() {
+        return exchangelogRepository.findAll();
+    }
+
     @RequestMapping(path = "/getByName")
     public @ResponseBody
     Iterable<Exchange> getByName(@RequestParam(value = "name") String name) {
         return exchangeRepository.findByName(name);
     }
+
+    @RequestMapping(path = "/getByRank")
+    public @ResponseBody
+    Iterable<Exchange> getByRank(@RequestParam(value = "rank") Integer rank) {
+        return exchangeRepository.findByRank(rank);
+    }
+
+    @RequestMapping(path = "/getlatest")
+    public @ResponseBody
+    Iterable<Exchange> getLatest() {
+        ArrayList<Exchangelog> logs = (ArrayList<Exchangelog>) getAllLog();
+        Integer len = logs.size();
+        Exchangelog latest = logs.get(len - 1);
+        return exchangeRepository.findByTimestampBetween(latest.getStart(), latest.getEnd());
+    }
+
+
+    //获取交易所概况数据
+    //表格：按最近一次排名，排名、（LOGO）、名字、过去24小时交易量、网址、排名曲线、交易量曲线
+
 }
